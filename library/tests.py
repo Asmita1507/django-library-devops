a4ae0dbad483ac2f562d479c2eeaa57ee695988a
harshada05044 <harshadac04@gmail.com>
2025-04-22 16:09:50 +0530

    Update tests.py

--- library/tests.py
+++ library/tests.py
@@ -1,65 +1,68 @@
 from django.test import TestCase
-  from django.urls import reverse
-  from library.models import Book, Borrower
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
+from library.models import Book, StudentExtra
 
-  class BookModelTest(TestCase):
-      def setUp(self):
-          self.book = Book.objects.create(
-              title="Simple Book",
-              author="Simple Author",
-              isbn="1234567890123",
-              published_date="2023-01-01",
-              genre="Fiction",
-              available_copies=5,
-              total_copies=5
-          )
+class BookModelTest(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(
+            name="Simple Book",
+            author="Simple Author",
+            isbn="1234567890123"
+        )
 
-      def test_book_creation(self):
-          """Test that a Book object is created with correct attributes."""
-          self.assertEqual(self.book.title, "Simple Book")
-          self.assertEqual(self.book.author, "Simple Author")
-          self.assertEqual(self.book.available_copies, 5)
+    def test_book_creation(self):
+        """Test that a Book object is created with correct attributes."""
+        self.assertEqual(self.book.name, "Simple Book")
+        self.assertEqual(self.book.author, "Simple Author")
+        self.assertEqual(self.book.isbn, "1234567890123")
 
-  class BorrowerModelTest(TestCase):
-      def setUp(self):
-          self.borrower = Borrower.objects.create(
-              name="Jane Doe",
-              email="jane@example.com",
-              phone_number="1234567890",
-              address="123 Main St"
-          )
+class StudentExtraModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='student', password='testpass')
+        self.student = StudentExtra.objects.create(
+            user=self.user,
+            enrollment="12345",
+            branch="CS"
+        )
 
-      def test_borrower_creation(self):
-          """Test that a Borrower object is created with correct attributes."""
-          self.assertEqual(self.borrower.name, "Jane Doe")
-          self.assertEqual(self.borrower.email, "jane@example.com")
+    def test_student_creation(self):
+        """Test that a StudentExtra object is created with correct attributes."""
+        self.assertEqual(self.student.enrollment, "12345")
+        self.assertEqual(self.student.branch, "CS")
+        self.assertEqual(self.student.user.username, "student")
 
-  class LibraryViewTest(TestCase):
-      def setUp(self):
-          self.book = Book.objects.create(
-              title="Simple Book",
-              author="Simple Author",
-              isbn="1234567890123",
-              published_date="2023-01-01",
-              genre="Fiction",
-              available_copies=5,
-              total_copies=5
-          )
-          self.borrower = Borrower.objects.create(
-              name="Jane Doe",
-              email="jane@example.com",
-              phone_number="1234567890",
-              address="123 Main St"
-          )
+class LibraryViewTest(TestCase):
+    def setUp(self):
+        # Create an admin user
+        self.user = User.objects.create_user(username='admin', password='testpass')
+        admin_group, _ = Group.objects.get_or_create(name='ADMIN')
+        self.user.groups.add(admin_group)
+        
+        # Log in the admin user
+        self.client.login(username='admin', password='testpass')
+        
+        # Create test data
+        self.book = Book.objects.create(
+            name="Simple Book",
+            author="Simple Author",
+            isbn="1234567890123"
+        )
+        self.student_user = User.objects.create_user(username='student', password='testpass')
+        self.student = StudentExtra.objects.create(
+            user=self.student_user,
+            enrollment="12345",
+            branch="CS"
+        )
 
-      def test_book_list_view(self):
-          """Test that the book list view returns 200 and uses correct template."""
-          response = self.client.get(reverse('book_list'))
-          self.assertEqual(response.status_code, 200)
-          self.assertTemplateUsed(response, 'library/book_list.html')
+    def test_viewbook_view(self):
+        """Test that viewbook_view returns 200 and uses correct template for admin."""
+        response = self.client.get(reverse('viewbook'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'library/viewbook.html')
 
-      def test_borrower_list_view(self):
-          """Test that the borrower list view returns 200 and uses correct template."""
-          response = self.client.get(reverse('borrower_list'))
-          self.assertEqual(response.status_code, 200)
-          self.assertTemplateUsed(response, 'library/borrower_list.html')
+    def test_viewstudent_view(self):
+        """Test that viewstudent_view returns 200 and uses correct template for admin."""
+        response = self.client.get(reverse('viewstudent'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'library/viewstudent.html')
